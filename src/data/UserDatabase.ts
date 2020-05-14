a948c18c2f68df407f11994a99f49651724596d7
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-05-14 15:42:50 -0300

    adiciona UserDatabase que pega o email e o id

---
+++ src/data/UserDatabase.ts
@@ -0,0 +1,26 @@
+import { BaseDatabase } from "./BaseDatabase";
+
+
+export class UserDatabase extends BaseDatabase {
+    private static TABLE_NAME = "User"
+
+    public async createUser(id: string, email: string, password: string): Promise<void> {
+        await this.setConnection()
+            .insert({
+                id,
+                email,
+                password
+            })
+            .into(UserDatabase.TABLE_NAME)
+    }
+
+    public async getUserByEmail(email: string): Promise<any> {
+        const result = await this.setConnection().raw(`SELECT * FROM ${UserDatabase.TABLE_NAME} WHERE email = "${email}"`)
+        return result[0][0]
+    }
+
+    public async getUserById(id: string): Promise<any> {
+        const result = await this.setConnection().select('*').from(UserDatabase.TABLE_NAME).where({ id })
+        return result[0]
+    }
+}
