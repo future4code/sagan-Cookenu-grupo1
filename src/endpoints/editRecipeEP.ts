4601620dcc263a929f808639252c5d393b4b0c40
Danilo Mourelle <danilomourelle@outlook.com>
2020-05-16 14:09:52 -0300

    criação dos arquivos de EP e criação das funções app no index.ts

---
+++ src/endpoints/editRecipeEP.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+import { BaseDatabase } from '../data/BaseDatabase'
+
+export const editeRecipeEP = async (req: Request, res: Response) => {
+  try {
+      
+      res.status(200).send({  })
+
+  }
+  catch (err) {
+      res.status(400).send({ message: err.message })
+  }
+  finally {
+      await BaseDatabase.desconnectDB()
+  }
+}
